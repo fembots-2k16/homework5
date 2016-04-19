71dc64e99fe791ce3593a39a47e1c9b87859baea
jake trower <cakeandturtles@gmail.com>
2016-04-19 18:20:21 -0500

    added pretty much mocked up controllergit add .!

--- nav.py
+++ nav.py
@@ -11,9 +11,30 @@ from move_base_msgs.msg import MoveBaseActionResult
 from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import PoseArray
 from apriltags_ros.msg import AprilTagDetectionArray
+from nav_msgs.msg import Odometry
+from frontier_exploration.msg import ExploreTaskAction, ExploreTaskGoal
 
+rate = None
+goal_client = None
+exploration_client = None
 goal_status = 0
 found_ids = {}
+pose = None
+
+def orientationToAngle(orient):
+    angel = 2*math.degrees(math.asin(orient))
+    if angel < 0:
+        angel += 360
+    return angel
+
+def angleToOrientation(angle):
+    #angle = angle % 360
+    mult = 1
+    if angle > 180: mult = -1
+    orient = math.sin(math.radians(angle/2))
+    if mult < 0:
+        orient = mult*orient
+    return orient
 
 def moveBaseActionResultHandler(data):
     global goal_status
@@ -21,50 +42,82 @@ def moveBaseActionResultHandler(data):
     #print "status: ", status
     #print data.result
     #http://docs.ros.org/fuerte/api/actionlib_msgs/html/msg/GoalStatus.html
-    pass
 
 def tagDetectionsHandler(data):
-    global found_ids
+    global found_ids, pose, goal_status, rate
+
     detections = data.detections
 
     for detection in detections:
         id = detection.id
-        if id in found_ids
-        size = detection.size
-        pose = detection.pose
+        if id not in found_ids:
+            size = detection.size
+            pose = detection.pose
+
+            #TODO:: how to interrupt exploration_client??
+
+            #TODO:: set up the goal pose!!!
+            x = 0 #???
+            y = 0 #???
+            z_theta = 0 #???
+
+            goal = MoveBaseGoal()
+            goal.target_pose.header.frame_id = "map"
+            goal.target_pose.header.stamp = rospy.get_rostime()
+
+            goal.target_pose.pose.position.x = x
+            goal.target_pose.pose.position.y = y
+            goal.target_pose.pose.orientation.z = z_theta
+
+            goal_client.send_goal(goal)
+
+            #wait for the goal to finish!!!
+            while goal_status < 3:
+                rate.sleep()
+
+            #TODO:: how do restart exploration_client?
+
+def odometryHandler(data):
+    global pose
+    pose = pose_msg.pose.pose
+
 
 def main():
-    global point
-    global status
-    print 'what... are your ending coordinates?'
+    global goal_client, exploration_client, rate
     rospy.init_node('homework4_navigator')
     rate = rospy.Rate(10)
+    rospy.Subscriber("/odom", Odometry, odometryHandler)
     rospy.Subscriber("/move_base/result", MoveBaseActionResult, moveBaseActionResultHandler)
     rospy.Subscriber("/tag_detections", AprilTagDetectionArray, tagDetectionsHandler)
-
-    while (point == None):
-       rate.sleep()
-
-    #TODO::
-    x = 0
-    y = 0
 
 
     velPub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
+    print "."
     print "make sure you start the motors!"
 
-    client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    goal_client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    exploration_client = actionlib.SimpleActionClient("explore_server", ExploreTaskAction)
 
     #Waits until the action server has started up and started listening for goals
-    client.wait_for_server()
+    goal_client.wait_for_server()
+    exploration_client.wait_for_server()
 
     #TODO
     time = 0
     three_minutes = 3
     # MAIN LOOP
     while time < three_minutes:
-        #NAVIGATE (USING FRONTIER EXPLORATION?)
+        exploration_goal = ExploreTaskGoal
+        exploration_goal.explore_boundary.header.seq = 1
+        exploration_goal.explore_boundary.header.frame_id = 1
+        exploration_goal.explore_center.point.x = 1
+        exploration_goal.explore_center.point.y = 0
+        exploration_goal.explore_center.point.z = 0
+
+        exploration_client.send_goal(goal)
+
+        #TODO:: how to interrupt exploration_client??
 
 
 if __name__ == "__main__":
